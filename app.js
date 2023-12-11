332091d7f7f7d84894f2bbdb36834cdddb6ca40e
Janhavi Banarase <96691214+JanhaviBanarase@users.noreply.github.com>
2023-12-11 23:40:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,58 +1,49 @@
 let newsBox = document.getElementById('newsBox');
 let spinner = document.getElementById('spinner');
-let newsCateogry = ['national', 'business', 'sports', 'world', 'politics', 'technology', 'startup', 'entertainment', 'miscellaneous', 'hatke', 'science', 'automobile'];
-
-const xhr = new XMLHttpRequest();
+let newsCategory = ['national', 'business', 'sports', 'world', 'politics', 'technology', 'startup', 'entertainment', 'miscellaneous', 'hatke', 'science', 'automobile'];
 
 function sendCategory(index) {
-    getNews(newsCateogry[index])
+    getNews(newsCategory[index]);
 }
+
 getNews("all");
 
-function getNews(newsCateogryName) {
-    xhr.open('GET', `https://inshortsapi.vercel.app/news?category=${newsCateogryName}`, true);
-
-    xhr.getResponseHeader('Content-type', 'application/json');
-
-    xhr.onload = function () {
+async function getNews(newsCategoryName) {
+    try {
+        const response = await fetch(`https://inshortsapi.vercel.app/news?category=${newsCategoryName}`);
         
-        if (this.status === 200) {
-            let json = JSON.parse(this.responseText);
-            let data = json.data;  
-            let newsHTML = "";
-
-            function showSpinner() {
-                spinner.style.visibility = 'hidden';
-                newsBox.style.visibility = 'visible';
-            }
-
-            xhr.onprogress = showSpinner();
-
-          
-            for (key in data) {
-
-                let news = `<div class="newsCard card">
-                            <img src="${data[key].imageUrl}"
-                                class="img ard-img-top img-thumbnail" alt="Image" style="height: 15rem;">
-                            <h5 class="card-header">${data[key].title}</h5>
-                            <div class="card-body">
-                                <h5 class="card-title">Author: ${data[key].author}</h5>
-                                <p class="card-text">${data[key].content}</p>
-                                <a target="_blank" href="${data[key].readMoreUrl}" class="btn btn-primary">Read more..</a>
-                            </div>
-                           <div class="text-center card-footer text-muted">${data[key].date}</div>
-                           
-                        </div>`;
-                newsHTML += news;
-            }
-          
-            newsBox.innerHTML = newsHTML;
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        else {
-            console.log("Some Error Occured");
+
+        const json = await response.json();
+        let data = json.data;
+        let newsHTML = "";
+
+        function showSpinner() {
+            spinner.style.visibility = 'hidden';
+            newsBox.style.visibility = 'visible';
         }
+
+        showSpinner();
+
+        for (const key in data) {
+            let news = `<div class="newsCard card">
+                        <img src="${data[key].imageUrl}"
+                            class="img ard-img-top img-thumbnail" alt="Image" style="height: 15rem;">
+                        <h5 class="card-header">${data[key].title}</h5>
+                        <div class="card-body">
+                            <h5 class="card-title">Author: ${data[key].author}</h5>
+                            <p class="card-text">${data[key].content}</p>
+                            <a target="_blank" href="${data[key].readMoreUrl}" class="btn btn-primary">Read more..</a>
+                        </div>
+                        <div class="text-center card-footer text-muted">${data[key].date}</div>
+                    </div>`;
+            newsHTML += news;
+        }
+
+        newsBox.innerHTML = newsHTML;
+    } catch (error) {
+        console.error("Some error occurred:", error.message);
     }
-
-    xhr.send();  
-
 }
